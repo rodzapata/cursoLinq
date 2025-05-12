2c4edb50ea438b52a61770a6d3e73d67a8598e7f
rodzapata <rodneyzapatapalacio@gmail.com>
2025-05-11 21:05:24 -0500

    add proyect SelectConsole

---
+++ MyproyectoConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+    /*
+        static IEnumerable<string> Personas()
+        {
+            yield return "rodney";
+            yield return "Cecilia Mercedes":
+            yield return "samuel David";
+            yield return "Juan Miguel";
+        }
+    */
+        Console.WriteLine("curso de linQ");
+        Console.WriteLine("query Syntax");
+        //Armamos el query
+
+        string[] Personas={"rodney","cecilia"};
+
+        IEnumerable<string> valores=from n in Personas
+                    select n;
+
+        /*
+        var personas = from p in Personas()
+                       select p;
+        */
+        //ejecutamos el Query a trabez de un ciclo
+        foreach (var p in valores)
+        {
+            Console.WriteLine(p);
+        }
+
+    }
+}
---
+++ SelectConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numeros =[1,2,3,4,5];
+        foreach (int numero  in numeros)
+        {
+            Console.WriteLine(numero);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
+
