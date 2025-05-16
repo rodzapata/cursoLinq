bf3c50a76e87f1ed137d9ea08ccd80553c0360a9
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2025-05-16 17:08:06 -0500

    add union

---
+++ Union/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Uniones en Linq");
+
+string[] nombres={"rodney","cecilia"};
+string[] apellidos={"zapata","cantillo"};
+
+/*
+Console.WriteLine("---syntax query sql");
+var nombresApellidos = (from n in nombres
+                    select n).Union(apellidos);
+*/
+
+System.Console.WriteLine("---syntax arrow funtion");
+var nombresApellidos = nombres.Select(n => n).Union(apellidos);
+
+foreach (var item in nombresApellidos)
+{
+    System.Console.WriteLine(item);
+}
